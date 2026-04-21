0b7dffb6538d27692f26975a529508c9437acc9a
sengupta9298 <sengupta9298@gmail.com>
2026-04-21 04:31:42 +0000

    03_05_SSG

--- src/main/java/com/example/StoreItem.java
+++ src/main/java/com/example/StoreItem.java
@@ -10,17 +10,13 @@ public class StoreItem
     private final Integer stockAvailable;
     private final String packagingType;
 
-    public StoreItem(String name, Double price, String shortDescription, String longDescription,
-                     Integer stockAvailable, String packagingType) {
-        this.name = name;
-        this.price = price;
-        this.shortDescription = shortDescription;
-        this.longDescription = longDescription;
-        this.stockAvailable = stockAvailable;
-        this.packagingType = packagingType;
-        if (name == null || price == null) {
-            throw new IllegalArgumentException("Name and price must not be null");
-        }
+    private StoreItem(ItemBuilder builder) {
+        this.name = builder.name;
+        this.price = builder.price;
+        this.shortDescription = builder.shortDescription;
+        this.longDescription = builder.longDescription;
+        this.stockAvailable = builder.stockAvailable;
+        this.packagingType = builder.packagingType;
     }
 
     @Override
@@ -34,4 +30,39 @@ public class StoreItem
                 ", packagingType='" + packagingType + '\'' +
                 '}';
     }
+    public static class ItemBuilder {
+        private  String name;
+        private  Double price;
+        private  String shortDescription;
+        private  String longDescription;
+        private  Integer stockAvailable;
+        private  String packagingType;
+
+        public ItemBuilder(String name, Double price) {
+            this.name = name;
+            this.price = price;
+        }
+
+        public ItemBuilder shortDescription(String shortDescription) {
+            this.shortDescription = shortDescription;
+            return this;
+        }
+        public ItemBuilder longDescription(String longDescription) {
+            this.longDescription = longDescription;
+            return this;
+        }
+        public ItemBuilder stockAvailable(int stockAvailable) {
+            this.stockAvailable = stockAvailable;
+            return this;
+        }
+        public ItemBuilder packagingType(String packagingType) {
+            this.packagingType = packagingType;
+            return this;
+        }
+
+        public StoreItem build() {
+            return new StoreItem(this);
+        } 
+        
+    } 
 }
